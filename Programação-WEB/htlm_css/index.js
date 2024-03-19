8bbbb18589854cc10628748c080063e4454b4101
Gustavo Almeida <estudante@ifpr.edu.br>
2024-03-18 21:03:57 -0300

    calculadora

---
+++ Programação-WEB/htlm_css/index.js
@@ -0,0 +1,5 @@
+function somar(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    let resultado = parseFloat(valor1)+parseFloat(valor2);
+}
