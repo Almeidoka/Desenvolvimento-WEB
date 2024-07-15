2291dd475c60188376a6c1d516606f405d2f9f24
Almeida <107077336+Almeidoka@users.noreply.github.com>
2024-07-14 21:55:01 -0300

    Ferramenta de alteracao de Tema + cópia de contato

--- Programação-WEB/htlm_css/index.js
+++ Programação-WEB/htlm_css/index.js
@@ -1,4 +1,4 @@
-let inputValor1 = document.getElementById("valor1");
+/*let inputValor1 = document.getElementById("valor1");
 inputValor1.addEventListener('mouseover',function(){
     alert("olá");
 });//SPAN BOX AO MOUSEOVER(PASSAR O MOUSE) NO CAMPO VALOR1
@@ -9,3 +9,34 @@ function somar(){
     let resultado = parseFloat(valor1)+parseFloat(valor2);
     document.getElementById("resultado").innerText=resultado;
 }
+*/
+document.addEventListener('DOMContentLoaded',()=>{
+    verificarTema();
+})
+
+function alterarTema(){
+    const tema = document.body.getAttribute("data-tema");
+    const novoTema = tema == 'dark' ? 'light' : 'dark';
+    document.body.setAttribute("data-tema", novoTema);
+    localStorage.setItem('tema', novoTema);
+}
+function verificarTema(){
+    const temaArmazenado = localStorage.getItem('tema');
+    if(temaArmazenado){
+        document.body.setAttribute('data-tema', temaArmazenado);
+    }
+}
+
+function copiar(){
+    const curriculo = document.getElementById("Contato").textContent;
+    const mostrarCurriculo = document.getElementById("Contato");
+    mostrarCurriculo.innerHTML='';
+    mostrarCurriculo.appendChild('16991951038');
+    navigator.clipboard.writeText(curriculo).then(()=>{
+        alert("Conteúdo copiado para a área de tranferência")
+    }).catch(error=>{
+        console.error("erro ao copiar", error);
+        alert("Erro ao copiar o conteúdo");
+    })
+}
+
