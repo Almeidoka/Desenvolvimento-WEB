51e184028fb7ed48c56c9bb458d1ffa9e5aac11c
Gustavo Almeida <estudante@ifpr.edu.com>
2024-07-29 20:45:21 -0300

    Adicionando funcoes

--- Programação-WEB/htlm_css/funcoes/calculadora/calculadora.js
+++ Programação-WEB/htlm_css/funcoes/calculadora/calculadora.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+    verificarTema();
 
 })
+
+function alterarTema() {
+    const tema = document.body.getAttribute("data-tema");
+    const novoTema = tema == 'dark' ? 'light' : 'dark';
+    document.body.setAttribute("data-tema", novoTema);
+    localStorage.setItem('tema', novoTema);
+}
+function verificarTema() {
+    const temaArmazenado = localStorage.getItem('tema');
+    if (temaArmazenado) {
+        document.body.setAttribute('data-tema', temaArmazenado);
+    }
+}
 
 function somar() {
     let valor1 = document.getElementById("valor1").value;
--- Programação-WEB/htlm_css/index.js
+++ Programação-WEB/htlm_css/index.js
@@ -1,15 +1,3 @@
-/*let inputValor1 = document.getElementById("valor1");
-inputValor1.addEventListener('mouseover',function(){
-    alert("olá");
-});//SPAN BOX AO MOUSEOVER(PASSAR O MOUSE) NO CAMPO VALOR1
-
-function somar(){
-    let valor1 = document.getElementById("valor1").value;
-    let valor2 = document.getElementById("valor2").value;
-    let resultado = parseFloat(valor1)+parseFloat(valor2);
-    document.getElementById("resultado").innerText=resultado;
-}
-*/
 document.addEventListener('DOMContentLoaded',()=>{
     verificarTema();
     
